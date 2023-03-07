da6a13b49e7e9a714d5e5b66a1a60ac9927bcbfc
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-03-08 00:40:39 +0300

    Add greeting messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = [
+  "Hello !!",
+  "Nihao !!",
+  "Kunichiwa !!",
+  "Marhaba !!",
+  "Ku !!"
+]
+
+greetings.each do |message|
+  Message.create(message: message)
+end
